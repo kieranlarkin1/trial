2c61955ad941a643d608d7b352e69e10117d45e2
kieranlarkin1 <kieranlarkin1@hotmail.com>
2020-07-20 23:54:58 +0100

    access beta?

--- mod_subrou.f90
+++ mod_subrou.f90
@@ -1,5 +1,7 @@
 MODULE mod_subrou
 
+  USE param
+
 IMPLICIT NONE
 
 REAL(8), PARAMETER :: phi = 4.d0
@@ -8,17 +10,12 @@ CONTAINS
 
 SUBROUTINE trial_sub(v,n,out)
 
-USE param
-
 IMPLICIT NONE
 
 INTEGER, INTENT(IN) :: n
 REAL(8), DIMENSION(n), INTENT(IN) :: v
 REAL(8), DIMENSION(n), INTENT(OUT) :: out
 REAL(8), PARAMETER :: theta = 2.d0
-REAL(8) :: beta
-
-beta = 0.9d0
 
 WRITE(*,*) ' SR: beta, phi, theta= ', beta, phi, theta
 out = (phi/theta) * V
