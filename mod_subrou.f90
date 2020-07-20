b4904376a1d5bf28f575a8520993b5e51b3c7b2f
kieranlarkin1 <kieranlarkin1@hotmail.com>
2020-07-21 00:09:15 +0100

    change beta in SR?

--- mod_subrou.f90
+++ mod_subrou.f90
@@ -16,6 +16,8 @@ INTEGER, INTENT(IN) :: n
 REAL(8), DIMENSION(n), INTENT(IN) :: v
 REAL(8), DIMENSION(n), INTENT(OUT) :: out
 REAL(8), PARAMETER :: theta = 2.d0
+REAL(8) :: beta
+beta = 0.9
 
 WRITE(*,*) ' SR: beta, phi, theta= ', beta, phi, theta
 out = (phi/theta) * V
