24ff36caf7b99f2770a5781008ac2a820a0d584a
kieranlarkin1 <kieranlarkin1@hotmail.com>
2020-07-25 16:59:08 +0100

    write csv

--- main.f90
+++ main.f90
@@ -90,10 +90,11 @@ IMPLICIT NONE
   ! Evaluate function value on nodes Sf: m functions
   CALL SPeval(csV, s, ns2, m, Sf, nf, order, Vf)
 
-  WRITE(*,*) ' FINISHED! '
+  WRITE(*,*) ' FINISHED! ', csV
   WRITE(*,*) ' iteration: ',t, ' tolerance: ', step
   WRITE(*,*) ' MA: beta, phi, theta= ', beta, phi, theta
   WRITE(*,*) ' V(0)= ', v0, v(1)
+  WRITE(*,*) ' Vf= ', v(round(na/2)), Vf(:,round(nf/2))
 
   OPEN (UNIT=25, FILE="Output.txt", ACTION="WRITE", POSITION="REWIND")
   WRITE(25,*) ' A ', ' V ',' POL ',' W ',' Vf '
