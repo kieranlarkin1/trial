5f124d842e9896abf5adc3cdcf890cedc652cf6f
kieranlarkin1 <kieranlarkin1@hotmail.com>
2020-07-29 00:18:52 +0100

    add risk back

--- param.f90
+++ param.f90
@@ -21,7 +21,7 @@ IMPLICIT NONE
     REAL(rk), PARAMETER :: mn_e = 0.d0
     REAL(rk), PARAMETER :: mx_e = 0.1d0
     INTEGER(ik), PARAMETER :: ne0 = ne/2 + 1              ! mean income level
-    REAL(rk), PARAMETER :: sigma_r = 5.d-10
+    REAL(rk), PARAMETER :: sigma_r = 5.d-3
 
 
     INTEGER(ik), PARAMETER :: mx_it = 10000
