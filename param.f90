50ed37c101d583d3ee41e4ab5dd6acaceceb22ac
kieranlarkin1 <kieranlarkin1@hotmail.com>
2020-07-28 19:45:16 +0100

    m=5

--- param.f90
+++ param.f90
@@ -19,7 +19,7 @@ IMPLICIT NONE
 
     INTEGER(ik), PARAMETER :: ne = 3
     REAL(rk), PARAMETER :: mn_e = 1.d-5
-    REAL(rk), PARAMETER :: mx_e = 1.d0
+    REAL(rk), PARAMETER :: mx_e = 0.5d0
     INTEGER(ik), PARAMETER :: ne0 = ne/2 + 1              ! mean income level
 
     INTEGER(ik), PARAMETER :: mx_it = 10000
