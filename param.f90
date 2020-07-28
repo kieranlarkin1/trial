f677ff3ad06466de6918c8a5cc3e7f5333bc7ff7
kieranlarkin1 <kieranlarkin1@hotmail.com>
2020-07-28 19:46:02 +0100

    m=5

--- param.f90
+++ param.f90
@@ -17,7 +17,7 @@ IMPLICIT NONE
     REAL(rk), PARAMETER :: mn_a = 0.d0
     REAL(rk), PARAMETER :: mx_a = 10.d0
 
-    INTEGER(ik), PARAMETER :: ne = 3
+    INTEGER(ik), PARAMETER :: ne = 5
     REAL(rk), PARAMETER :: mn_e = 1.d-5
     REAL(rk), PARAMETER :: mx_e = 0.5d0
     INTEGER(ik), PARAMETER :: ne0 = ne/2 + 1              ! mean income level
@@ -30,7 +30,7 @@ IMPLICIT NONE
 ! Spline parameters
   INTEGER(ik), PARAMETER :: ns=25                    ! total number of knots, including endpoints, on S
   CHARACTER(30), PARAMETER :: indicator = "not-a-knot"  ! complete or natural spline endpoint condition
-  INTEGER(ik), PARAMETER :: m=3              ! Number of functions to evaluate.
+  INTEGER(ik), PARAMETER :: m=5             ! Number of functions to evaluate.
 
   INTEGER(ik), PARAMETER :: nf=na                    ! total number of function evaluations
   INTEGER(ik), PARAMETER :: order=0                 ! 0: value, 1: first derivative, 2: 2nd derivative
