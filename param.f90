d6180407d4f5d70d0b6c8d5afb110282b77b04ee
kieranlarkin1 <kieranlarkin1@hotmail.com>
2020-07-29 00:09:15 +0100

    param

--- param.f90
+++ param.f90
@@ -8,7 +8,7 @@ IMPLICIT NONE
     REAL(rk), PARAMETER :: beta=0.989d0
     REAL(rk), PARAMETER :: r=0.01d0
 
-    REAL(rk), PARAMETER :: omega=1.d-3    ! transfer
+    REAL(rk), PARAMETER :: omega=1.d-4    ! transfer
     REAL(rk), PARAMETER :: pi = 0.85d0      ! Persistence
 
     REAL(rk), PARAMETER :: vmin = -1.d10
@@ -19,7 +19,7 @@ IMPLICIT NONE
 
     INTEGER(ik), PARAMETER :: ne = 5
     REAL(rk), PARAMETER :: mn_e = 0.d0
-    REAL(rk), PARAMETER :: mx_e = 0.25d0
+    REAL(rk), PARAMETER :: mx_e = 0.1d0
     INTEGER(ik), PARAMETER :: ne0 = ne/2 + 1              ! mean income level
     REAL(rk), PARAMETER :: sigma_r = 5.d-10
 
