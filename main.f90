72aa816730ab83efc4897c584d7d7f05887f6075
kieranlarkin1 <kieranlarkin1@hotmail.com>
2020-07-31 16:04:31 +0100

    log e

--- main.f90
+++ main.f90
@@ -40,11 +40,11 @@ IMPLICIT NONE
   ! risk = (/ (-sigma_r+i*step, i = 0, ne-1) /)
 
 !  CALL linspace(mn_a, mx_a, na, a)
-  CALL linspace(mn_e, mx_e, ne, e)
+!  CALL linspace(mn_e, mx_e, ne, e)
   CALL linspace(-sigma_r, sigma_r, ne, risk)
 
    CALL logspace(mn_a, mx_a, na, a)
-  ! CALL logspace(mn_e, mx_e, ne, e)
+   CALL logspace(mn_e, mx_e, ne, e)
   ! CALL logspace(-sigma_r, sigma_r, ne, risk)
 
   v0 = log(omega+e(ne0))/(1-beta)
